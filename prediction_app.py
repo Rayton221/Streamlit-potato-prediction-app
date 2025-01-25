2e332f639a2ab16a6aa21ef38994eda2159d2efe
Rayton221 <137079569+Rayton221@users.noreply.github.com>
2025-01-25 22:53:44 +0300

    Add files via upload

--- prediction_app.py
+++ prediction_app.py
@@ -3,7 +3,6 @@ import streamlit as st
 from PIL import Image
 import numpy as np
 
-#model = tf.keras.models.load_model("E:\\DL\\Potatoe Project\\models\\version1.h5")
 model = tf.keras.models.load_model("version1.h5")
 classes = ["Early blight","Late blight","Healthy"]
 
